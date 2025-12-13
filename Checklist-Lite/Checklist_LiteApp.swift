af02a0669a3b9a8ff6a518001672990a8660586f
Tesfaldet Haileab <tus29603@temple.edu>
2025-12-12 23:14:03 -0500

    Initial Commit

---
+++ Checklist-Lite/Checklist_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checklist_LiteApp.swift
+//  Checklist-Lite
+//
+//  Created by Tesfaldet Haileab on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Checklist_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
